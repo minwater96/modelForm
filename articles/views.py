a8ad917a66533552f41a0b3abc88c5e80a7be788
minwater96 <rlaalstn2568@naver.com>
2024-07-31 12:16:53 +0900

    create(ver.1)기능구현

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,37 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+
+def create(request):
+    # new/ => 빈 종이를 보여주는 기능
+    # create/ => 사용자가 입력한 데이터를 저장
+
+    # ==========
+
+    # GET create/ => 빈 종이를 보여주는 기능
+    # POST create/ => 사용자가 입력한 데이터를 저장
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid(): # 데이타를 제대로 입력했는지 확인
+            form.save() # True값 저장
+            return redirect('articles:index')
+        else:
+            form = ArticleForm(request.POST) #기존 데이터를 가지고 새로운 폼 제작
+
+            context = {
+                'form': form,
+
+            }
+        return render(request, 'create.html', context)
+
+    else:
+        form = ArticleForm()
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'create.html', context)
